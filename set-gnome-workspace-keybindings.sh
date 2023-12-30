1d38f241a14c226c4ff5357426b3d4b742b78705
Mukesh Sonkar <mkso13189@gmail.com>
2023-12-30 19:42:51 +0530

    Added more configs

---
+++ set-gnome-workspace-keybindings.sh
@@ -0,0 +1,14 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<alt>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<alt>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<alt>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<alt>4']"
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<alt><shift>1']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<alt><shift>2']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<alt><shift>3']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<alt><shift>4']"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<alt>j']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<alt>k']"
+
+gsettings set org.gnome.desktop.wm.keybindings close "['<super><shift>q']"
